81f8afc675b784d1a0576118544e6b3fcc0d7aae
Gael <gaelsegura007@gmail.com>
2026-01-26 13:45:57 -0600

    Add Reflection and Transmission probabilities

---
+++ R_T_coef.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+m = 9.11e-31 # electron mass
+hbar = 6.58e-16 # Planck's constant (eV.s)
+E = 10.0 # Energy (eV)
+V = 9.0 # Energy (eV)
+
+# Compute wave numbers
+k1 = sqrt(2.0*m*E)/hbar
+k2 = sqrt(2.0*m*(E-V))/hbar
+
+# Transmission coefficient
+T = 4.0*k1*k2/((k1+k2)**2)
+
+# Reflection coefficient
+R = ((k1-k2)/(k1+k2))**2
+
+print("Transmission = ", T)
+print("Reflection = ", R)
+print()
+print(T+R)
