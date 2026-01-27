e57908462b937d0d8820805542dd52d30308dc9a
Gael <gaelsegura007@gmail.com>
2026-01-27 11:53:30 -0600

    Add def function

--- plot_exp_noise.py
+++ plot_exp_noise.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0,2*np.pi,400)
 
-mu = 0.0
-std = 0.1
-size = 400
-y_rand = np.random.normal(mu, std, size)
+def func_noise(a, b, mu, std, size):
+	x = np.linspace(a, b, size)
+	y_x = np.exp(-x)
+	y_noise = np.random.normal(mu, std, size)
+	y = y_x + y_noise
+	return x, y_x, y
 
-y = np.exp(-x) + y_rand
+x, y_x, y = func_noise(0, 10, 0.0, 0.1, 100)
 
+plt.plot(x, y_x, 'r-')
 plt.plot(x, y)
-plt.plot(x, np.exp(-x))
 plt.show()
