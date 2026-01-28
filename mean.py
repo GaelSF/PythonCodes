5d29b478dfe41bdd62995b97d3af53d66eeb03bf
Gael <gaelsegura007@gmail.com>
2026-01-28 12:40:41 -0600

    Random list comprehension

--- mean.py
+++ mean.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-list_num = [1,2,3,4,5]
+list_num = [np.random.rand() for i in range(4)]
+print(list_num)
+#list_num = [1,2,3,4,5]
 m=np.mean(list_num)
 print(m)
