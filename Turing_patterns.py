1621c708c4126bab7a5cc4ce8d956b9843330f25
Gael <gaelsegura007@gmail.com>
2026-01-29 18:54:26 -0600

    Reaction-Diffusion code

---
+++ Turing_patterns.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+a = 2.8e-4 
+b = 5e-3
+tau = .1
+k = -0.005
+
+size = 80
+dx = 2./size
+T = 10.0
+dt = 0.9*dx**2/2
+n = int(T/dt)
+print(n)
+
+U = np.random.rand(size,size)
+V = np.random.rand(size,size)
+
+def laplacian(Z):
+	Ztop = Z[0:-2,1:-1]
+	Zleft = Z[1:-1,0:-2]
+	Zbottom = Z[2:,1:-1]
+	Zright = Z[1:-1,2:]
+	Zcenter = Z[1:-1,1:-1]
+	Ztot = Ztop + Zleft + Zbottom + Zright - 4*Zcenter
+	Ztot = Ztot/dx**2
+	return Ztot
+
+for i in range(n):
+	deltaU = laplacian(U)
+	deltaV = laplacian(V)
+
+	Uc = U[1:-1,1:-1]	
+	Vc = V[1:-1,1:-1]
+
+	U[1:-1,1:-1] = Uc + dt*(a*deltaU + Uc - Uc**3 - Vc + k)	
+	V[1:-1,1:-1] = Vc + dt*(b*deltaV + Uc - Vc )/tau
+
+	for Z in (U,V):
+		Z[0,:] = Z[1,:]
+		Z[-1,:] = Z[-2,:]
+		Z[:,0] = Z[:,1]
+		Z[:,-1] = Z[:,-2]
+
+#	if (i // 10000) == 0:
+		
+#		plt.imshow(U, cmap=plt.cm.copper, extent=[-1,1,-1,1])
+#		plt.title(f"Frame: {i}")
+#		plt.show()	
+plt.imshow(U, cmap=plt.cm.copper, extent=[-1,1,-1,1])
+plt.savefig("turing_pattern.png")
+#plt.show()	
